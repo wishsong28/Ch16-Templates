940791bbf40019930aa119066ca16947b92beaf8
wishs <wishs@LAPTOP-VF7D5Q0F>
2025-12-07 17:31:26 -0500

    Functional Program

--- Ch16 Templates/Source.cpp
+++ Ch16 Templates/Source.cpp
@@ -6,10 +6,15 @@
 //This program demonstrates the use of function templates by accepting a numerical values and returning the value halved.
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 //Template definition for half function
-
+template <typename T>
+T half(T value)
+{
+	return value / 2;
+}
 
 int main()
 {
@@ -24,3 +29,7 @@ int main()
 	return 0;
 }
 
+int half<int>(int value)
+{
+	return static_cast<int>(round(value / 2.0));
+}
