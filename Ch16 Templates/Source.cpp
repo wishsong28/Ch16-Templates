d7b7e6cdfc42cee1e9f1319d6779a831b9b5c462
wishs <wishs@LAPTOP-VF7D5Q0F>
2025-12-07 17:15:16 -0500

    Completed function main provided by professor

--- Ch16 Templates/Source.cpp
+++ Ch16 Templates/Source.cpp
@@ -3,4 +3,24 @@
 //Ch16 Templates - Programming Assignment #15b
 //12.07.2025
 
-//This program demonstrates the use of function templates by accepting a numerical value from the user and returning the value halved.
+//This program demonstrates the use of function templates by accepting a numerical values and returning the value halved.
+
+#include <iostream>
+using namespace std;
+
+//Template definition for half function
+
+
+int main()
+{
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+
+	return 0;
+}
+
