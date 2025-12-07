610664f1cd0f2d4e8bc2992ee82a0b8d17191180
wishs <wishs@LAPTOP-VF7D5Q0F>
2025-12-07 17:04:43 -0500

    Add project files.

---
+++ Ch16 Templates/Source.cpp
@@ -0,0 +1,6 @@
+//Rebekah Bisbee
+//25.FA.CIS.1202.501 C++ Software Development
+//Ch16 Templates - Programming Assignment #15b
+//12.07.2025
+
+//This program demonstrates the use of function templates by accepting a numerical value from the user and returning the value halved.
